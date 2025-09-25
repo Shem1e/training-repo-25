f9c115db06d64cddd370c8391f7ab25d1f93ea9f
Shem1e <vldsadchikovv@gmail.com>
2025-09-25 09:31:47 +0300

    Fix USER_NAME error in Shem1e/GitHubUserStatistics.java

--- Shem1e/GitHubUserStatistics.java
+++ Shem1e/GitHubUserStatistics.java
@@ -5,7 +5,7 @@ import java.net.URL;
 
 
 public class GitHubUserStatistics {
-    public final static String USER_NAME;
+    public final static String USER_NAME = "<github_username>";
 
 
     public static void main(String[] args) {
